cb96db37a681610d6181f90422cffbc400054e14
Nicky <nickysauceofficial@gmail.com>
2022-12-06 17:38:26 +0530

    event capturing, trickling, stop event bubbling/capturing

--- Event Propagation/EventsPropagation.js
+++ Event Propagation/EventsPropagation.js
@@ -2,17 +2,24 @@ const divEl = document.querySelector(".div__container");
 const formEl = document.querySelector(".form__container");
 const btnEl = document.querySelector(".btn__click");
 
-divEl.addEventListener("click", alertFunc);
-formEl.addEventListener("click", alertFunc);
-btnEl.addEventListener("click", alertFunc);
+divEl.addEventListener("click", alertFunc, {
+  capture: true,
+});
+formEl.addEventListener("click", alertFunc, {
+  capture: true,
+});
+btnEl.addEventListener("click", alertFunc, {
+  capture: true,
+});
 
 function alertFunc(e) {
+  e.stopPropagation();
   alert(
-    "currentTarget " +
+    "currentTarget: " +
       e.currentTarget.tagName +
-      " target " +
+      ", target: " +
       e.target.tagName +
-      " this " +
+      ", this: " +
       this.tagName
   );
 }
