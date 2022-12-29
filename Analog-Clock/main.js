8dfc8aef29a5c01f3301f6ba79ef735e67c48a36
Nicky <nickysauceofficial@gmail.com>
2022-12-29 21:29:11 +0530

    alanog watch changes

--- Analog-Clock/main.js
+++ Analog-Clock/main.js
@@ -1,6 +1,7 @@
 const minutesEl = document.querySelector(".minutes__hand");
 const hoursEl = document.querySelector(".hour__hand");
 const secondsEl = document.querySelector(".seconds__hand");
+const currentTimeEl = document.querySelector(".curr__time");
 
 setInterval(() => {
   showTime();
@@ -12,9 +13,11 @@ function showTime() {
   let seconds = new Date().getSeconds();
 
   hours = hours > 12 ? hours - 12 : hours;
+  currentTimeEl.innerHTML = `${hours} : ${mins} : ${seconds}`;
+  seconds = seconds * 6 - mins * 6;
 
   hoursEl.style.transform = `rotate(calc(-90deg + ${hours * 30}deg))`;
   minutesEl.style.transform = `rotate(${mins * 6 - hours * 30}deg)`;
-  secondsEl.style.transform = `rotate(-${mins * 6 - seconds * 6}deg)`;
+  secondsEl.style.transform = `rotate(${seconds}deg)`;
 }
 showTime();
