3c88c0a99684096e6cf46370afe8a4857efd82d9
Nicky <nickysauceofficial@gmail.com>
2022-12-29 15:44:05 +0530

    toast

---
+++ Toast/main.js
@@ -0,0 +1,71 @@
+const toast = document.querySelector(".toast__container");
+
+const positionEl = document.getElementById("position");
+const statusEl = document.getElementById("status");
+
+const buttonEl = document.querySelector(".btn__styles");
+
+buttonEl.addEventListener("click", showToast);
+
+function showToast(e) {
+  switch (positionEl.value) {
+    case "topLeft":
+      {
+        toast.style.left = 0;
+        toast.style.top = 0;
+      }
+      break;
+    case "topRight":
+      {
+        toast.style.right = 0;
+        toast.style.top = 0;
+      }
+      break;
+    case "bottomLeft":
+      {
+        toast.style.left = 0;
+        toast.style.bottom = 0;
+      }
+      break;
+    case "bottomRight":
+      {
+        toast.style.right = 0;
+        toast.style.bottom = 0;
+      }
+      break;
+  }
+
+  switch (statusEl.value) {
+    case "warning":
+      {
+        toast.innerText = "Warning";
+        toast.style.backgroundColor = "Yellow";
+      }
+      break;
+    case "success":
+      {
+        toast.innerText = "Success";
+        toast.style.backgroundColor = "Green";
+      }
+      break;
+    case "failed":
+      {
+        toast.innerText = "Failed";
+        toast.style.backgroundColor = "Red";
+      }
+      break;
+    case "loading":
+      {
+        toast.innerText = "Loading";
+        toast.style.backgroundColor = "White";
+      }
+      break;
+  }
+
+  clearInterval();
+
+  toast.classList.toggle("hidden");
+  setTimeout(() => {
+    toast.classList.toggle("hidden");
+  }, 2000);
+}
