747d320a81d972c874c120fb17c25d3c38b9bbe8
Nicky <nickysauceofficial@gmail.com>
2023-01-05 18:29:55 +0530

    merged

--- DND-Swap-Add-Delete/main.js
+++ DND-Swap-Add-Delete/main.js
@@ -1,6 +1,6 @@
-const gridContainer = document.querySelector('.grid__container');
+const gridContainer = document.querySelector(".grid__container");
 
-const gridSize = document.querySelector('#gridSize');
+const gridSize = document.querySelector("#gridSize");
 
 function renderGrid() {
   gridContainer.innerHTML = ``;
@@ -12,20 +12,20 @@ function renderGrid() {
   for (let i = 1; i <= gridSz; i++) {
     for (let j = 1; j <= gridSz; j++) {
       const className = `${i}_${j}`;
-      const InputBox = document.createElement('div');
-      const FileInput = document.createElement('input');
-      const AddSign = document.createElement('div');
-      AddSign.classList.add('add__sign');
-      AddSign.innerText = '+';
+      const InputBox = document.createElement("div");
+      const FileInput = document.createElement("input");
+      const AddSign = document.createElement("div");
+      AddSign.classList.add("add__sign");
+      AddSign.innerText = "+";
       InputBox.appendChild(AddSign);
-      FileInput.type = 'file';
-      FileInput.classList.add('class', 'input__box');
+      FileInput.type = "file";
+      FileInput.classList.add("class", "input__box");
       InputBox.appendChild(FileInput);
       InputBox.classList.add(className);
-      InputBox.classList.add('file__container');
-      InputBox.style.setProperty('--ht-wt', ht_wt);
+      InputBox.classList.add("file__container");
+      InputBox.style.setProperty("--ht-wt", ht_wt);
 
-      FileInput.addEventListener('change', (e) => {
+      FileInput.addEventListener("change", (e) => {
         // console.log(e.target.files[0]);
         fetchImgAndDisplay(FileInput, InputBox);
       });
@@ -35,19 +35,19 @@ function renderGrid() {
 }
 renderGrid();
 
-gridSize.addEventListener('input', renderGrid);
+gridSize.addEventListener("input", renderGrid);
 
 function fetchImgAndDisplay(file, element) {
   const imgFile = file.files[0];
 
-  const IMGBOX = document.createElement('div');
-  IMGBOX.classList.add('image__box');
-  const DeleteBtn = document.createElement('div');
-  DeleteBtn.classList.add('delete__btn');
-  DeleteBtn.classList.add('hidden');
-  DeleteBtn.innerText = 'X';
-  const IMG = document.createElement('img');
-  IMG.classList.add('image__styles');
+  const IMGBOX = document.createElement("div");
+  IMGBOX.classList.add("image__box");
+  const DeleteBtn = document.createElement("div");
+  DeleteBtn.classList.add("delete__btn");
+  DeleteBtn.classList.add("hidden");
+  DeleteBtn.innerText = "X";
+  const IMG = document.createElement("img");
+  IMG.classList.add("image__styles");
 
   const reader = new FileReader();
   reader.onloadend = function () {
@@ -55,31 +55,31 @@ function fetchImgAndDisplay(file, element) {
   };
 
   if (imgFile) {
-    element.children[0].classList.add('hidden');
-    element.children[1].classList.add('hidden');
+    element.children[0].classList.add("hidden");
+    element.children[1].classList.add("hidden");
     reader.readAsDataURL(imgFile);
     IMGBOX.appendChild(DeleteBtn);
     IMGBOX.appendChild(IMG);
     element.appendChild(IMGBOX);
 
-    IMGBOX.addEventListener('mouseenter', (e) => {
-      e.target.children[0].classList.remove('hidden');
+    IMGBOX.addEventListener("mouseenter", (e) => {
+      e.target.children[0].classList.remove("hidden");
     });
-    IMGBOX.addEventListener('mouseleave', (e) => {
-      e.target.children[0].classList.add('hidden');
+    IMGBOX.addEventListener("mouseleave", (e) => {
+      e.target.children[0].classList.add("hidden");
     });
 
-    DeleteBtn.addEventListener('click', (e) => {
-      element.children[0].classList.remove('hidden');
-      element.children[1].classList.remove('hidden');
-      element.children[2].children[1].src = '';
+    DeleteBtn.addEventListener("click", (e) => {
+      element.children[0].classList.remove("hidden");
+      element.children[1].classList.remove("hidden");
+      element.children[2].children[1].src = "";
       element.children[2].children[1].remove();
       element.children[2].children[0].remove();
       IMGBOX.remove();
-      IMGBOX.removeEventListener('mouseenter', () => {
+      IMGBOX.removeEventListener("mouseenter", () => {
         DeleteBtn.remove();
       });
-      IMGBOX.removeEventListener('mouseleave', () => {
+      IMGBOX.removeEventListener("mouseleave", () => {
         DeleteBtn.remove();
       });
     });
